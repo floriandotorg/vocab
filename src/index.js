27f1a0bb489ca4ab95ac01f8f3933bf2f3e4ee53
Florian Kaiser <mail@floriankaiser.org>
2021-02-07 22:09:57 +0100

    add authentication

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './components/app'
+import firebase from 'firebase/app'
 import './style.scss'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+firebase.auth().signInAnonymously().then(() => {
+  ReactDOM.render(<App />, document.getElementById('root'))
+})
+.catch((error) => {
+  console.error(error)
+})
